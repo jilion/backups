68ba27464b721962998c88c912dc1ff9928e63bb
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-02-06 10:43:28 +0100

    Comment sublimevideo_mongohq backup

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 # Not used for now (Heroku Scheduler Addons don't support more than 30s jobs)
-namespace :backup do
-  task :sublimevideo_mongohq do
-    `bundle exec backup perform -t sublimevideo_mongohq -c config.rb`
-  end
-end
+# namespace :backup do
+#   task :sublimevideo_mongohq do
+#     `bundle exec backup perform -t sublimevideo_mongohq -c config.rb`
+#   end
+# end
--- clock.rb
+++ clock.rb
@@ -2,9 +2,9 @@ require 'backup'
 
 include Clockwork
 
-every(24.hours, nil) do
-  system 'bundle exec backup perform -t sublimevideo_mongohq -c config.rb'
-end
+# every(24.hours, nil) do
+#   system 'bundle exec backup perform -t sublimevideo_mongohq -c config.rb'
+# end
 
 every(6.hours, nil) do
   %w[sublimevideo_pg aelios_mongohq jilion_mongohq jilion_freckle].each do |backup|
