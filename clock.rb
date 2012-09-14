4eabc95e8a559d009a3f6e333042fddc04cf3ad7
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-09-14 11:16:28 +0200

    Change log_path

--- clock.rb
+++ clock.rb
@@ -3,11 +3,11 @@ require 'backup'
 include Clockwork
 
 every(24.hours, nil) do
-  system 'bundle exec backup perform -t sublimevideo_stats_mongohq -c config.rb -q'
+  system 'bundle exec backup perform -t sublimevideo_stats_mongohq -c config.rb -l .'
 end
 
 every(6.hours, nil) do
   %w[sublimevideo_pg aelios_mongohq jilion_mongohq jilion_freckle].each do |backup|
-    system "bundle exec backup perform -t #{backup} -c config.rb -q"
+    system "bundle exec backup perform -t #{backup} -c config.rb -l ."
   end
 end
