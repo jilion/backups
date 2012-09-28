ad2cf08caf5c42d2e1e68c2b496787bc64bea15e
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-09-28 10:37:11 +0200

    Only sv pg for testing

--- clock.rb
+++ clock.rb
@@ -2,12 +2,16 @@ require 'backup'
 
 include Clockwork
 
-every(24.hours, nil) do
-  system 'bundle exec backup perform -t sublimevideo_stats_mongohq -c config.rb -l .'
-end
+# every(24.hours, nil) do
+#   system 'bundle exec backup perform -t sublimevideo_stats_mongohq -c config.rb -l .'
+# end
+
+# every(6.hours, nil) do
+#   %w[sublimevideo_pg aelios_mongohq jilion_mongohq jilion_freckle].each do |backup|
+#     system "bundle exec backup perform -t #{backup} -c config.rb -l ."
+#   end
+# end
 
 every(6.hours, nil) do
-  %w[sublimevideo_pg aelios_mongohq jilion_mongohq jilion_freckle].each do |backup|
-    system "bundle exec backup perform -t #{backup} -c config.rb -l ."
-  end
+  system "bundle exec backup perform -t sublimevideo_pg -c config.rb -l ."
 end
